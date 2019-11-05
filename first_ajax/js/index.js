abf81f22a29451a7cb4455668535a4334c84b4d4
Zachary <zachjoliver@gmail.com>
2019-11-04 21:53:29 -0800

    Finish first project

--- first_ajax/js/index.js
+++ first_ajax/js/index.js
@@ -3,10 +3,10 @@ console.log("Hello from the JavaScript console!");
 // Your AJAX request here
 
 $.ajax({
-  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=san%20francisco,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
   type: 'GET',
-  success: function(message) {
-    console.log(message);
+  success: function(payload) {
+    console.log(payload.weather);
   },
   error: function(errorMessage) {
     console.log(errorMessage)
